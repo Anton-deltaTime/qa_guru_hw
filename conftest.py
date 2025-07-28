2023902a92c87b989951b212f010f689029b6eeb
Anton Yaroslavtsev <a.yaroslavtsev@rt-solar.ru>
2025-07-28 16:27:29 +0300

    done home work 3

---
+++ conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+@pytest.fixture(scope='module', autouse=True)
+def setup():
+    # Добавление заголовков (иногда достаточный минимум для адекватной реакции браузера на тесты)
+    options = webdriver.ChromeOptions()
+    options.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36')
+    options.add_argument('Content-Type=text/plain;charset=UTF-8')
+    options.add_argument('Connection=keep-alive')
+    browser.config.driver_options = options
+
+    browser.config.window_width = 1024
+    browser.config.window_height = 768
+    yield
+    browser.quit()
---
+++ test_home_work/test_hw_3.py
@@ -0,0 +1,26 @@
+import random
+import string
+
+from selene import browser, be, by, have
+
+
+def test_search_true():
+    browser.open('https://ya.ru/')
+    # Проблема showcaptcha решается переоткрыванием вкладки
+    if browser.element('html').wait_until(have.text('Подтвердите, что запросы отправляли вы, а не робот')):
+        browser.open('https://ya.ru/')
+    browser.element(by.id('text')).should(be.blank)
+    browser.element(by.id('text')).type('python selene').press_enter()
+    browser.element(by.id('search-result')).should(have.text('yashaka/selene: User-oriented Web UI browser tests'))
+
+
+def test_search_false():
+    browser.open('https://ya.ru/')
+    # Проблема showcaptcha решается переоткрыванием вкладки
+    if browser.element('html').wait_until(have.text('Подтвердите, что запросы отправляли вы, а не робот')):
+        browser.open('https://ya.ru/')
+    browser.element(by.id('text')).should(be.blank)
+    # Генерация случайного набора символов ascii длинной 15
+    random_text = ''.join(random.choices(string.ascii_letters, k=15))
+    browser.element(by.id('text')).type(random_text).press_enter()
+    browser.element('html').should(have.text('Ничего не нашли'))
