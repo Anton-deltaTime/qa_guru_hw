507f0af8339582b1e2844911780f7a3c1bed5af2
Anton Yaroslavtsev <a.yaroslavtsev@rt-solar.ru>
2025-09-27 22:48:37 +0300

    add attach

--- conftest.py
+++ conftest.py
@@ -1,28 +1,61 @@
+import os
 import pytest
 import subprocess
 
+from dotenv import load_dotenv
 from selene import browser
 from selenium import webdriver
+
+from utils import attach
 
 
 def pytest_addoption(parser):
     parser.addoption("--loc", action="store", default="local", choices=['local', 'jenkins'], help="'local' or 'jenkins', test launch location")
 
 
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
+
+
 @pytest.fixture(scope='module')
-def setup():
-    # Добавление заголовков (иногда достаточный минимум для адекватной реакции браузера на тесты)
+def setup(request):
+    get_location = request.config.getoption("--loc")
+
     options = webdriver.ChromeOptions()
+
+    if get_location == 'jenkins':
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": "128.0",
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True,
+                "enableLog": True
+            }
+        }
+        options.capabilities.update(selenoid_capabilities)
+
+        driver = webdriver.Remote(
+            command_executor=f"https://{os.getenv("SELENOID_LOGIN")}:{os.getenv("SELENOID_PASSWORD")}@selenoid.autotests.cloud/wd/hub",
+            options=options)
+        browser.config.driver = driver
+
     options.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36')
     options.add_argument('Content-Type=text/plain;charset=UTF-8')
     options.add_argument('Connection=keep-alive')
     options.add_argument('--ignore-certificate-errors-spki-list')
     options.add_argument('--ignore-ssl-errors')
-    browser.config.driver_options = options
 
+    browser.config.driver_options = options
     browser.config.window_width = 1600
     browser.config.window_height = 1000
     yield
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
     browser.quit()
 
 
