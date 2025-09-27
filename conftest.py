124521573f82e1bd4997c46f7d393a89799f2d03
Anton Yaroslavtsev <a.yaroslavtsev@rt-solar.ru>
2025-09-28 00:31:26 +0300

    fix attach

--- conftest.py
+++ conftest.py
@@ -24,6 +24,11 @@ def setup(request):
     get_location = request.config.getoption("--loc")
 
     options = webdriver.ChromeOptions()
+    options.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36')
+    options.add_argument('Content-Type=text/plain;charset=UTF-8')
+    options.add_argument('Connection=keep-alive')
+    options.add_argument('--ignore-certificate-errors-spki-list')
+    options.add_argument('--ignore-ssl-errors')
 
     if get_location == 'jenkins':
         selenoid_login = os.getenv("SELENOID_LOGIN")
@@ -44,12 +49,6 @@ def setup(request):
             command_executor=f"https://{selenoid_login}:{selenoid_password}@selenoid.autotests.cloud/wd/hub",
             options=options)
         browser.config.driver = driver
-
-    options.add_argument('User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36')
-    options.add_argument('Content-Type=text/plain;charset=UTF-8')
-    options.add_argument('Connection=keep-alive')
-    options.add_argument('--ignore-certificate-errors-spki-list')
-    options.add_argument('--ignore-ssl-errors')
 
     browser.config.driver_options = options
     browser.config.window_width = 1600
