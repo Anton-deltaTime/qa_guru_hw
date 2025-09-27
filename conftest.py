2efbd93244f0c172f303ea220f2f718125bddd1e
Anton Yaroslavtsev <a.yaroslavtsev@rt-solar.ru>
2025-09-28 01:18:17 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -36,7 +36,7 @@ def setup(request):
         with allure.step('Настройка selenoid для options'):
             selenoid_capabilities = {
                 "browserName": "chrome",
-                "browserVersion": "128.0",
+                "browserVersion": "127.0",
                 "selenoid:options": {
                     "enableVNC": True,
                     "enableVideo": True,
